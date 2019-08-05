b6dcdb122c26f3de8a92edb44460fa3fc0a5132c
odur <odurjoseph8@gmail.com>
2019-08-05 17:53:35 +0300

    add token generator

---
+++ api/utils/token_generator.py
@@ -0,0 +1,60 @@
+from flask import request
+from functools import wraps
+import jwt
+from decouple import config
+from base64 import b64decode
+from .exceptions import ValidationError
+
+def token_required(func):
+
+    @wraps(func)
+    def decorated(*args, **kwargs):
+        token = get_token()
+
+        try:
+            decoded_token = jwt.decode(
+                token,
+                b64decode(config('JWT_PUBLIC_KEY')).decode('utf8'),
+                algorithms=['RS256'],
+                options={
+                    'verify_signature': True,
+                    'verify_exp': True
+                })
+        except (ValueError, TypeError, jwt.ExpiredSignatureError,
+                jwt.DecodeError, jwt.InvalidSignatureError):
+            raise ValidationError({'Invalid token'})
+
+        return func(*args, **kwargs)
+
+    return decorated
+
+def generate_token(user, exp=None):
+    payload = {
+        'UserInfo': user,
+    }
+    payload.__setitem__('exp', exp) if exp is not None else ''
+
+    token = jwt.encode(payload, b64decode(config('JWT_PRIVATE_KEY')).decode('utf8'), algorithm='RS256').decode('utf-8')
+    return token
+
+
+def get_token(http_request=request):
+    """Get token from request object
+
+    Args:
+        http_request (HTTPRequest): Http request object
+
+    Returns:
+        token (string): Token string
+
+    Raises:
+        ValidationError: Validation error raised when there is no token
+                         or bearer keyword in authorization header
+    """
+    token = http_request.headers.get('Authorization')
+    if not token:
+        raise ValidationError({'message': 'NO_TOKEN_MSG'}, 401)
+    elif 'bearer' not in token.lower():
+        raise ValidationError({'message': 'NO_BEARER_MSG'}, 401)
+    token = token.split(' ')[-1]
+    return token
