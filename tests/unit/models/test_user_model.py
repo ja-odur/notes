d0146e63ba56fc9debdce9460c57db2fc96d0705
odur <odurjoseph8@gmail.com>
2019-08-06 14:04:35 +0300

    test user model

--- tests/unit/models/test_user_model.py
+++ tests/unit/models/test_user_model.py
@@ -8,6 +8,19 @@ test_user_data = dict(
 
 class TestUserModel(TestCase):
 
+    def test_very_passowrd(self):
+        user_data = dict(test_user_data)
+        password_hash = User.generate_hash(test_user_data['password'])
+        user_data['password'] = password_hash
+        user = User(**user_data)
+
+        assert user.verify_password(test_user_data['password'])
+
+    def test_serializer(self):
+        user = User(**test_user_data).serialize()
+
+        assert user['first_name'] == test_user_data['first_name']
+
     def test_representation(self):
         user = User(**test_user_data)
 
