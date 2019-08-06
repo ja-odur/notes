cde9e6b4f8e17800b7a5167558216084481d5718
odur <odurjoseph8@gmail.com>
2019-08-06 09:07:02 +0300

    update note schema

--- api/models/note.py
+++ api/models/note.py
@@ -4,5 +4,12 @@ from .model_operations import ModelOperation
 
 class Note(db.Document, ModelOperation):
     title = db.StringField(required=True, primary_key=True, max_length=80)
+    email = db.EmailField(required=True)
     body = db.StringField(required=True)
     shared = db.ListField()
+
+    def serialize(self):
+        return dict(title=self.title, body=self.body)
+
+    def __repr__(self):
+        return '<Note: {}>'.format(self.title)
