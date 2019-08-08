981a82a317b92cff9a56580962ceb74d3696d5e5
odur <odurjoseph8@gmail.com>
2019-08-08 10:56:04 +0300

    rewrite user endpoint tests using fixtures

---
+++ tests/test_user_endpoints.py
@@ -0,0 +1,53 @@
+from tests.test_base_case import BaseTestCase
+import json
+
+test_user_data = dict(
+        email='Test@email.com', first_name='testfirstName', last_name='testLastName', password='test'
+    )
+
+invalid_test_user_data = dict(
+        email='Test@email.com', first_name='testfirstName', last_name='testLastName', password='invalid_password'
+    )
+
+
+class TestUserLoginEndpointsFix:
+
+    def test_signup_succeeds(self, client, init_db):
+        res = client().post("/api/v1/user", content_type="application/json", data=json.dumps(test_user_data))
+
+
+
+        response = json.loads(res.data.decode('utf-8'))
+
+        assert res.status_code == 201
+        assert response['status'] == 'success'
+
+    def test_duplicate_signup_fails(self, client, init_db):
+        res = client().post("/api/v1/user", content_type="application/json", data=json.dumps(invalid_test_user_data))
+
+        response = json.loads(res.data.decode('utf-8'))
+
+        assert res.status_code == 400
+        assert response['status'] == 'error'
+
+    def test_login_succeeds(self, client, init_db):
+        res = client().post("/api/v1/user/login", content_type="application/json", data=json.dumps(test_user_data))
+
+
+        response = json.loads(res.data.decode('utf-8'))
+
+        assert res.status_code == 200
+        assert response['status'] == 'success'
+
+
+    def test_login_with_invalid_data_fails(self, client, init_db):
+        res = client().post(
+            "/api/v1/user/login",
+            content_type="application/json",
+            data=json.dumps(invalid_test_user_data))
+
+
+        response = json.loads(res.data.decode('utf-8'))
+
+        assert res.status_code == 401
+        assert response['status'] == 'error'
